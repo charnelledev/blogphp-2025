cea3652f264e1b7f7c5ad268dc3ec931c5c8a101
charnelledev <kengnemaiva54@gmail.com>
2025-04-24 12:25:03 +0100

    article

---
+++ admin.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'database/database.php';
+
+// 1--On affiche le titre autre
+
+$pageTitle ='Page Admin'; 
+
+// 2-Debut du tampon de la page de sortie
+ 
+ob_start();
+
+// 3-inclure le layout de la page d' accueil
+require_once 'layouts/adminqwerty/admin_dashboardqwerty_html.php';
+
+//4-recuperation du contenu du tampon de la page d'accueil
+$pageContent = ob_get_clean();
+
+//5-Inclure le layout de la page de sortie
+require_once 'layouts/layout_html.php';
